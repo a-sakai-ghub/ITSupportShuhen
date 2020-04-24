44d0c27db02b4c7319ec60768acdfe48547fe1fb
kanri_aforce <kanri_aforce@DESKTOP-MFFNFFQ>
2020-04-24 16:54:13 +0900

    初コミット

---
+++ src/CsvOutputTool.java
@@ -0,0 +1,14 @@
+/**
+ * CSV出力ツール用期待値算出ツール。
+ * @author kanri_aforce
+ *
+ */
+
+public class CsvOutputTool {
+	/**
+	 * 期待値算出メソッド。
+	 */
+	public static void ExpectCalculate() {
+
+	}
+}
---
+++ src/SintyokuKanriHaneiTool.java
@@ -0,0 +1,14 @@
+/**
+ * 進捗管理反映ツール用期待値算出ツール。
+ * @author kanri_aforce
+ *
+ */
+public class SintyokuKanriHaneiTool {
+	/**
+	 * 期待値算出メソッド。
+	 */
+	public static void ExpectCalculate() {
+
+	}
+
+}
