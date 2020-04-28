e85bbee7ab57d2a65ace16a26e0e9fa5fdcf0538
s.wakabayashi <s.wakabayashi@a-force.co.jp>
2020-04-28 10:24:03 +0900

    2020/04/28 s.wakabayashi メイン処理のパッケージ移動

---
+++ src/main/CsvOutputTool.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * CSV出力ツール用期待値算出ツール。
+ * @author kanri_aforce
+ *
+ */
+
+public class CsvOutputTool {
+	/**
+	 * 期待値算出メソッド。藤原テスト
+	 */
+	public static void expectCalculate(String input1, String input2) {
+
+	}
+}
---
+++ src/main/SintyokuKanriHaneiTool.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * 進捗管理反映ツール用期待値算出ツール。
+ * @author kanri_aforce
+ *
+ */
+public class SintyokuKanriHaneiTool {
+	/**
+	 * 期待値算出メソッド。
+	 */
+	public static void expectCalculate(String input1, String input2) {
+
+	}
+
+}
